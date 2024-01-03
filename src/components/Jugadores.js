432d2b8289770732ff72b4f438ecf5710eff8bb8
agulujan41 <agulujan41proj@gmail.com>
2024-01-03 12:04:52 -0300

    git fixed 2

--- src/components/Jugadores.js
+++ src/components/Jugadores.js
@@ -3,10 +3,11 @@ import { useParams } from "react-router-dom";
 import {Cursor,useTypewriter} from 'react-simple-typewriter'
 import "./styles/style.css";
 const Jugadores = () => {
+    
    
   const jugadores = {
     "GVM13026":{ nombre: "Guilmer", apellido: "Valencia Montes", puntos: 3.9,url:"https://i.ibb.co/DLv7Rzh/cr7.png" , desc:"Programador en Python - desarrollador de Software",team:"club de cr7" ,updated:'03-01-2024 00:00:00'},
-    "JHSB13231":{ nombre: "Jhon H", apellido: "Sejas B", puntos: 8,url:"https://i.ibb.co/HhgdS6p/taekwondo.png", desc:"Programador en Python - desarrollador de Software",team:"tae kwon do club",updated:'03-01-2024 00:00:00' },
+    "JHSB13231":{ nombre: "Jhon Hilton", apellido: "Sejas B", puntos: 8,url:"https://i.ibb.co/Kjp4rG9/taek.png", desc:"Programador en Python - desarrollador de Software",team:"tae kwon do club",updated:'03-01-2024 00:00:00' },
   };
   const { idPlayer } = useParams();
   const [detailsTitle] =useTypewriter({
